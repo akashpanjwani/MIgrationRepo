0cf7841d4fdc4800fe5f4e86b28585591dcd6138
akashpanjwani <akshaypanjwani94@gmail.com>
2019-07-03 02:06:16 +0530

    adding files

---
+++ Dkbs-MigrationTest-master/TestDKBSapis/TestDKBSapis/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.SharePoint.Client;
+using SP = Microsoft.SharePoint.Client;
+
+namespace Microsoft.SDK.SharePointServices.Samples
+{
+    class RetrieveListItems
+    {
+       public static void TestMethod1()
+        {
+            string siteUrl = "http://MyServer/sites/MySiteCollection";
+
+            ClientContext clientContext = new ClientContext(siteUrl);
+            clientContext.AuthenticationMode = ClientAuthenticationMode.FormsAuthentication;
+            clientContext.FormsAuthenticationLoginInfo = new FormsAuthenticationLoginInfo("CRM Automation", "9LEkTny4");
+            clientContext.ExecuteQuery();
+            SP.List oList = clientContext.Web.Lists.GetByTitle("Service katalog");
+
+            CamlQuery camlQuery = new CamlQuery();
+            camlQuery.ViewXml = "<View><Query></Query></View>";
+            ListItemCollection collListItem = oList.GetItems(camlQuery);
+
+            clientContext.Load(collListItem);
+
+            clientContext.ExecuteQuery();
+
+            // get partner listitem using this line, but you can use caml query to get many partner items
+
+            //ListItem partnerItem = lstCIs.GetItemById(lv.LookupId);
+
+            // get subsite url
+
+            //if (partnerItem["CISite"] != null)
+            //    {
+            //        FieldUrlValue subSiteUrl = partnerItem["CISite"] as FieldUrlValue;
+            //        using (ClientContext subsiteContext = new ClientContext(subSiteUrl.Url))
+            //        { }
+            //    }
+
+            foreach (ListItem oListItem in collListItem)
+            {
+                Console.WriteLine("ID: {0} \nTitle: {1} ", oListItem.Id, oListItem["Title"]);
+            }
+        }
+    }
+}
---
+++ Dkbs-MigrationTest-master/TestDKBSapis/TestDKBSapis/Program.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace TestDKBSapis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string siteUrl = "https://bookon.dkbs.dk/";
+
+            ClientContext clientContext = new ClientContext(siteUrl);
+            clientContext.AuthenticationMode = ClientAuthenticationMode.FormsAuthentication;
+            clientContext.FormsAuthenticationLoginInfo = new FormsAuthenticationLoginInfo("CRM Automation", "9LEkTny4");
+            clientContext.ExecuteQuery();
+            // string contentTypeName = "Bookinger";
+            Console.WriteLine(" Successfully Connected");
+            List oList = clientContext.Web.Lists.GetByTitle("Bookinger");
+            ListItemCollectionPosition itemPosition = null;
+            ContentTypeCollection ctColl = oList.ContentTypes;
+            clientContext.Load(ctColl);
+            clientContext.ExecuteQuery();
+            foreach (ContentType ct in ctColl)
+            {
+                if (ct.Name == "Bookinger" || ct.Name == "Gamle sager")
+                {
+                    Console.WriteLine(ct.Name);
+                    Console.WriteLine("---Please wait while list item is displaying.---");
+                    try
+                    {
+                        {
+                            string contentTypeName = "Bookinger";
+                            var query = new CamlQuery()
+                            {
+                                
+                                 ViewXml = String.Format("<View Scope='RecursiveAll'><Query><Where><Eq><FieldRef Name='ContentType' /><Value Type='Computed'>{0}</Value></Eq></Where></Query></View>", contentTypeName)
+                                // ViewXml = String.Format("<View Scope='RecursiveAll'><RowLimit>5000</RowLimit></View>")
+
+                            };
+
+                            ListItemCollection collListItem = oList.GetItems(query);
+
+                            clientContext.Load(collListItem);
+
+                            clientContext.ExecuteQuery();
+                            itemPosition = collListItem.ListItemCollectionPosition;
+                            Console.WriteLine(itemPosition);
+
+                            foreach (ListItem oListItem in collListItem)
+                            {
+                                Console.WriteLine("ID: {0} \nTitle: {1} ", oListItem.Id, oListItem["Title"]);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
